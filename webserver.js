4fa936732f6bd66a1bb000a08ce48378ad90212d
tykita <4rchive7@gmail.com>
2020-10-12 16:16:06 +0900

    first commit

---
+++ webserver.js
@@ -0,0 +1,11 @@
+const http = require('http'); // To use module, use requires method!
+
+const hostname = "127.0.0.1";
+const port = 1337;
+
+http.createServer((req, res)=>{
+    res.writeHead(200, { 'Content-Type': 'Text/plain' });
+    res.end('Hello World!');
+}).listen(port, hostname, () =>{
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
